3ab9f33ec025af017a8c19612f6d49f3e0d810e1
artem <artem.en9902@gmail.com>
2025-03-02 22:09:15 +0300

    add some files

--- CMake/run.bat
+++ CMake/run.bat
@@ -5,22 +5,23 @@ set BUILD_TYPE=Ninja
 set BUILD_SUFFIX=ninja
 
 set BUILD_FOLDER=build_%BUILD_SUFFIX%
-set SOURCE_FOLDER=project
+set SOURCE_FOLDER=projects
 
 if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
 
 cd %BUILD_FOLDER%
 
 cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
+
 cmake --build .
 
-if exist test (
-    cd test
-    ctest --output-on-failure
-    cd ..
+if exist tests (
+	cd tests
+	ctest --output-on-failure
+	cd ..
 )
 
 cd ..
 
-echo === Сборка и тестирование завершены! ===
-pause   
+echo === complete ===
+pause
