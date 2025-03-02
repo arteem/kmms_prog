97fbe863c256dbafaeb578217473f97b503f132e
artem <artem.en9902@gmail.com>
2025-03-02 19:55:46 +0300

    add some files

--- projects/sort/main.cpp
+++ projects/sort/main.cpp
@@ -1,15 +1,18 @@
-#include "io.hpp"
-#include "sortings.hpp"
+#include "source/io.hpp"
+#include "source/sortings.hpp"
+#include "source/generate.hpp"
 
-int main() {
-    int arr[] = {34, 76, 12, 54, 98, 45};
-	const int arr_size = sizeof(arr) / sizeof(arr[0]);
-	
-	eaa::print_array("Array before sort:", arr, arr_size);
-    
-    eaa::shell::shell_sort(arr, arr_size);
+int main()
+{
+    const int arr_size = mmh::io::input_user_size();
+    int* arr = new int[arr_size];
+    const int max_value = mmh::io::input_user_max_value();
 
-    eaa::print_array("Array after sort:", arr, arr_size);
+    mmh::io::print_array("Массив до сортировки:", arr, arr_size);
+
+    mmh::sortings::insert_sort(arr, arr_size);
+
+    mmh::io::print_array("Массив после сортировки:", arr, arr_size);
 
     return 0;
 }
